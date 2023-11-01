76c975f99d06d5c0e6a973b086bc9fac326a23fb
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-11-02 01:20:51 +0530

    LinearSearchList complete

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -2,12 +2,10 @@ export default function linear_search(
     haystack: number[],
     needle: number,
 ): boolean {
-    let value = false;
-    haystack.map((i) => {
-        if (i === needle) {
-            value = true;
-            return;
-        } else return;
-    });
-    return value;
+    for (let i = 0; i <= haystack.length; i++) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
 }
